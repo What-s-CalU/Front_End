193e6bcc42f374b76f8564e584c352d5b2a6aef6
thomasterhune <122999506+thomasterhune@users.noreply.github.com>
2023-01-31 18:10:25 -0500

    UI LOGIN PAGE

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Login Page'),
     );
   }
 }
@@ -48,16 +50,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  bool _showPassword = false;
+  void _togglevisibility() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _showPassword = !_showPassword;
     });
   }
 
@@ -69,16 +65,16 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    Color mainColor = Color(0xff083c74);
     return Scaffold(
+      //Remove elevation and of appbar and remove the extra title portion
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: mainColor,
+        toolbarHeight: 0,
+        elevation: 0,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
+      body: ListView(children: <Widget>[
+        Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -95,21 +91,162 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            //LOGO
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+              child: Container(
+                color: mainColor,
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                  child: Image(
+                    height: 200,
+                    width: 200,
+                    image: AssetImage('assets/Logo2.png'),
+                  ),
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            //USERNAME INPUT
+            Padding(
+              padding: const EdgeInsets.fromLTRB(40, 0, 40, 5),
+              child: TextField(
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.account_circle),
+                    hintText: "Username",
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            //PASSWORD INPUT
+            Padding(
+              padding: const EdgeInsets.fromLTRB(40, 5, 40, 5),
+              child: TextField(
+                obscureText: !_showPassword,
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock_open),
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        _togglevisibility();
+                      },
+                      child: Icon(
+                        _showPassword ? Icons.visibility : Icons.visibility_off,
+                      ),
+                    ),
+                    hintText: "Password",
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            //FORGOT PASSWORD
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 40, 20),
+              child: Align(
+                  alignment: Alignment.topRight,
+                  child: GestureDetector(
+                    onTap: () {
+                      print('Forgot Password...');
+                    },
+                    child: Text(
+                      "Forgot Password?",
+                    ),
+                  )),
+            ),
+            //LOGIN BUTTON
+            Padding(
+              padding: const EdgeInsets.fromLTRB(60, 0, 60, 10),
+              child: SizedBox(
+                width: double.infinity,
+                height: 40,
+                child: TextButton(
+                  onPressed: () {
+                    print('Loggin in');
+                  },
+                  style: TextButton.styleFrom(backgroundColor: mainColor),
+                  child: Text(
+                    'SIGN IN',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            //SIGN UP BUTTON
+            Padding(
+              padding: const EdgeInsets.fromLTRB(60, 0, 60, 10),
+              child: SizedBox(
+                width: double.infinity,
+                height: 40,
+                child: TextButton(
+                  onPressed: () {
+                    print('Loggin in');
+                  },
+                  style: TextButton.styleFrom(backgroundColor: mainColor),
+                  child: Text(
+                    'SIGN UP',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            //ROW FOR ICONS
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                //WEBSITE ICON
+                Column(
+                  children: [
+                    IconButton(
+                      iconSize: 40,
+                      onPressed: () {},
+                      icon: Icon(Icons.web, color: mainColor),
+                    ),
+                    Text(
+                      'Website',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                //ABOUT ICON
+                Column(
+                  children: [
+                    IconButton(
+                      iconSize: 40,
+                      onPressed: () {},
+                      icon: Icon(Icons.announcement_rounded, color: mainColor),
+                    ),
+                    Text(
+                      'About',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                //HELP ICON
+                Column(
+                  children: [
+                    IconButton(
+                      iconSize: 40,
+                      onPressed: () {},
+                      icon: Icon(Icons.question_mark, color: mainColor),
+                    ),
+                    Text(
+                      'Help',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ]),
     );
   }
 }
