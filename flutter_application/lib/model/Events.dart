ca255782ff7390cae4ea893a2f5d8efbc4276536
thomasterhune <122999506+thomasterhune@users.noreply.github.com>
2023-03-13 19:57:58 -0400

    changed appointmentBuilder based on calendar view type

--- flutter_application/lib/model/Events.dart
+++ flutter_application/lib/model/Events.dart
@@ -15,40 +15,42 @@ class Event {
     required this.color,
   });
 }
-  // Widget appointmentBuilder(BuildContext context, CalendarAppointmentDetails details) {
-  //   final Event event = details.appointments.first;
 
-  //   return Container(
-  //     width: details.bounds.width,
-  //     height: details.bounds.height,
-  //     decoration: BoxDecoration(
-  //       color: event.color.withOpacity(0.5),
-  //       border: Border.all(
-  //         color: event.color,
-  //         width: 2,
-  //       ),
-  //       borderRadius: BorderRadius.circular(12),
-  //     ),
-  //     child: Column(
-  //       mainAxisAlignment: MainAxisAlignment.center,
-  //       children: [
-  //         Text(
-  //           '${DateFormat.jm().format(event.startTime)} - ${DateFormat.jm().format(event.endTime)}',
-  //           style: TextStyle(
-  //             color: Colors.black,
-  //             fontSize: 12,
-  //             fontWeight: FontWeight.bold,
-  //           ),
-  //         ),
-  //         SizedBox(height: 4),
-  //         Text(
-  //           event.subject,
-  //           style: TextStyle(
-  //             color: Colors.black,
-  //             fontSize: 14,
-  //           ),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // }
+Widget appointmentBuilder(BuildContext context, CalendarAppointmentDetails details, CalendarController controller) {
+  final Event event = details.appointments.first;
+
+  return Container(
+    width: details.bounds.width,
+    height: details.bounds.height,
+    decoration: BoxDecoration(
+      color: event.color.withOpacity(0.5),
+      border: Border.all(
+        color: event.color,
+        width: 2,
+      ),
+      borderRadius: BorderRadius.circular(12),
+    ),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        if (controller.view == CalendarView.month)
+          Text(
+            '${DateFormat.jm().format(event.startTime)} - ${DateFormat.jm().format(event.endTime)}',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        SizedBox(height: 4),
+        Text(
+          event.subject,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 14,
+          ),
+        ),
+      ],
+    ),
+  );
+}
--- flutter_application/lib/pages/HomePage.dart
+++ flutter_application/lib/pages/HomePage.dart
@@ -5,10 +5,10 @@ import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:flutter/material.dart';
 import '../model/eventDataSource.dart';
 import '../pageUtility/homePageUtil.dart';
+import 'package:flutter_application_1/model/Events.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({super.key});
-  final CalendarController _controller = CalendarController();
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -16,6 +16,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Color mainColor = const Color(0xff083c74);
+  final CalendarController _calendarController = CalendarController();
 
   @override
   Widget build(BuildContext context) {
@@ -31,6 +32,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Expanded(
             child: SfCalendar(
               view: CalendarView.day,
+              controller: _calendarController,
               allowedViews: const [
                 CalendarView.day,
                 CalendarView.week,
@@ -39,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
               viewHeaderStyle: ViewHeaderStyle(),
               initialDisplayDate: DateTime.now(),
               dataSource: EventDataSource(events),
-              //appointmentBuilder: appointmentBuilder,
+              appointmentBuilder: (context, details) => appointmentBuilder(context, details, _calendarController),
               monthViewSettings: const MonthViewSettings(
                 showAgenda: true,
               ),
