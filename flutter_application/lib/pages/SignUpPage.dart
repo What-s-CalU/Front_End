10590b4576970825cada2b4cda4c39d5cafd7069
FDAapproved <32183732+FDAapproved@users.noreply.github.com>
2023-03-28 01:15:54 -0400

    Signup Page Progress
    Implemented:
    - states for each signup step (email, checksum, password),
    
    - error message for invalid requests,
    - basic state machine for controlling the page's interface.
    
    Unimplemented:
    - page currently does not refresh dynamically; only when reloading the page via sleep mode/etc. Might have to do with the way that the visibility() object is defined? I'm not sure how to force a widget reload, though that would likely be the ideal here (forcing the entire page's widgets to reload).
    
    - Error message currently has no formatting other than text color; it's not centered and looks a bit garish.
    
    - There is no special continue button for a user that has signed up, but not claimed their checksum (and exited out of the app). My plan is to send a unique json value back for this, or just a different html code to reveal a special button/enter a special state to skip steps.

--- flutter_application/lib/pages/SignUpPage.dart
+++ flutter_application/lib/pages/SignUpPage.dart
@@ -1,3 +1,5 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pageUtility/signUpPageUtil.dart';
 import '../httpRequests/httpRequests.dart';
@@ -22,8 +24,26 @@ class MySignUpPage extends StatefulWidget
 
 class _MySignUpPageState extends State<MySignUpPage>
 {
-  final TextEditingController _emailController      = TextEditingController();
+  final TextEditingController _emailController    = TextEditingController();
+  final TextEditingController _checksumController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  // final TextEditingController _passwordMirrorController = TextEditingController();
   Color mainColor = const Color(0xff083c74);
+  
+  // Control toggles for Visibility() scopes below.
+  bool   showEmailField    = true;
+  bool   showChecksumField = false;
+  bool   showPasswordField = false;
+  String buttonText        = "SUBMIT";
+  int    screenState       = 0;
+
+  bool   hadError          = false; 
+  String errorText         = "";
+
+  // Set this from another context to change the contents of the page. 
+  int    isReset           = 0;
+
+
 
   @override
   Widget build(BuildContext context)
@@ -44,7 +64,71 @@ class _MySignUpPageState extends State<MySignUpPage>
             <Widget>
             [
               buildLogoWidget(),
-              buildInputTextField(_emailController, "Email"),
+              Visibility
+              (
+                maintainSize:
+                  true,
+                maintainAnimation: 
+                  true,
+                maintainState:
+                  true,
+                visible: 
+                  hadError,
+                child:
+                  Text
+                    (
+                      // text field (see above).
+                      errorText,
+                      style:
+                        const TextStyle
+                        (
+                        color: Colors.red,
+                        ),
+                    ),
+              ),
+ 
+              // Conditionally hides whatever is inside of it. Used for all text fields. 
+              Visibility
+              (
+                maintainSize:
+                  true,
+                maintainAnimation: 
+                  true,
+                maintainState:
+                  true,
+                visible: 
+                  showEmailField,
+                child:
+                  buildInputTextField(_emailController, "Email"),
+              ),
+              // Conditionally hides whatever is inside of it. Used for all text fields. 
+              Visibility
+              (
+                maintainSize:
+                  true,
+                maintainAnimation: 
+                  true,
+                maintainState:
+                  true,
+                visible: 
+                  showChecksumField,
+                child:
+                  buildInputTextField(_checksumController, "Code"),
+              ),
+              // Conditionally hides whatever is inside of it. Used for all text fields. 
+              Visibility
+              (
+                maintainSize:
+                  true,
+                maintainAnimation: 
+                  true,
+                maintainState:
+                  true,
+                visible: 
+                  showPasswordField,
+                child:
+                  buildInputTextField(_passwordController, "Password"),
+              ),
               Padding
               (
                 padding:
@@ -62,41 +146,95 @@ class _MySignUpPageState extends State<MySignUpPage>
                         onPressed:
                         ()async
                         {
-                          int statcode =
-                          await
-                          (
-                            sendSignUpStart
-                            (
-                                0,
-                                _emailController.text
-                            )
-                          );
-                        if(statcode == 200)
-                        {
-                          print("yay we sign up");
-                          setState
-                          (
-                            ()
-                            {
-                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => MyHomePage()));
-                            }
-                          );
-                        }
+
+                          switch(screenState)
+                          {
+                            case 0:
+                              int statcode = await(sendSignUpStart(0, _emailController.text));
+                              if(statcode == 200)
+                              {
+                                showEmailField    = false;
+                                showChecksumField = true;
+                                buttonText        = "VERIFY";
+                                screenState       = 1;
+                                hadError          = false;
+                                print("Began sign up successfully.\n");
+                              }
+                              else if(statcode == 401)
+                              {
+                                errorText = "This email is already registered; please contact \"whatsatcalu@gmail.com\" for more information.";
+                                hadError  = true;
+                              }
+                              else
+                              {
+                                errorText = "Something went wrong; please try submitting again later.";
+                                hadError  = true;
+                              }
+                              break;
+
+
+                            case 1:
+                              int statcode = await(sendSignUpContinue(0, _emailController.text, _checksumController.text));
+                              if(statcode == 200)
+                              {
+                                showEmailField    = false;
+                                showChecksumField = false;
+                                buttonText        = "SIGN UP";
+                                screenState       = 2;
+                                hadError          = false;
+                                print("Exchanged checksum successfully.\n");
+
+                              }
+                              else if(statcode == 401)
+                              {
+                                errorText = "Incorrect code entered; doublecheck the email that was sent to your inbox at \"${_emailController.text.toLowerCase()}@pennwest.edu.\"";
+                                hadError  = true;
+                              }
+                              else
+                              {
+                                errorText = "Something went wrong; please try submitting again later.";
+                                hadError  = true;
+                              }
+                              break;
+
+
+                            case 2:
+                              // check for same passwords goes here ... 
+                              int statcode = await(sendSignUpEnd(0, _emailController.text, _passwordController.text));
+                              if(statcode == 200)
+                              {
+                                screenState = 0;
+                                hadError    = false;
+                                print("Signed up successfully.\n");
+                                
+                                // Builds the home page. This probably should boot you back to the login screen instead. 
+                                setState((){Navigator.of(context).push(MaterialPageRoute(builder: (context) => const MyHomePage()));});
+                              }
+                              else
+                              {
+                                errorText = "Something went wrong; please try submitting again later.";
+                                hadError  = true;
+                              }
+                              break;
+                          }
+
                       },
                       style:
                         TextButton.styleFrom(backgroundColor: mainColor),
                       child:
-                        const Text
+                        Text
                         (
-                          'SIGN UP',
+                          // text field (see above).
+                          buttonText,
                           style:
-                            TextStyle
+                            const TextStyle
                             (
                             color: Colors.white,
                             ),
                         ),
                       ),
                   ),
+ 
               ),
             ],
           ),
